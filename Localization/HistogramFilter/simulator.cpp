51b66f407137923a1667d4b4a15afe0aa2a114fa
Daniel Ingram <ingramds@appstate.edu>
2019-02-27 20:31:01 -0500

    Draw robot on probability grid

--- Localization/HistogramFilter/simulator.cpp
+++ Localization/HistogramFilter/simulator.cpp
@@ -7,7 +7,7 @@ Simulator::Simulator(int m, int n, float pSensor, float pMove) : m(m), n(n), pSe
     map = vector<vector<bool> >(m, vector<bool>(n, false));
     initMap(map);
 
-    pMap = cv::Mat(m, n, CV_32F);
+    pMap = cv::Mat(m, n, CV_32FC1);
     filter = Histogram(m, n, pSensor, pMove);
     filter.initBelief(pMap);
 
@@ -29,6 +29,9 @@ void Simulator::initMap(vector<vector<bool> >& map)
 int Simulator::run()
 {
     bool Z;
+    double minVal, maxVal;
+    cv::Point minLoc, maxLoc;
+    cv::Mat img(m, n, CV_32FC1);
     vector<int> d(2);
     while (true)
     {
@@ -36,7 +39,12 @@ int Simulator::run()
         d = robot.move();
         filter.sense(Z, map, pMap);
         filter.move(d, pMap);
-        cv::imshow("Histogram Filter", 100000*pMap);
+        cv::minMaxLoc(pMap, &minVal, &maxVal, &minLoc, &maxLoc);
+        cv::cvtColor(1-pMap/maxVal, img, cv::COLOR_GRAY2BGR);
+        cv::circle(img, cv::Point(robot.y, robot.x), 1, cv::Scalar(0, 0, 255), -1);
+        std::cout << "Actual robot location:    [" << robot.y << ", " << robot.x << "]\n"
+                     "Estimated robot location: " << maxLoc << std::endl;
+        cv::imshow("Histogram Filter", img);
         if (cv::waitKey(1) == 27)
             break;
     }
