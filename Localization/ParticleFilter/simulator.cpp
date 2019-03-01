0eb5500512f7d077c2a01ec335d03870baef7796
Daniel Ingram <ingramds@appstate.edu>
2019-02-28 20:22:00 -0500

    Initialize landmarks and particles

--- Localization/ParticleFilter/simulator.cpp
+++ Localization/ParticleFilter/simulator.cpp
@@ -3,6 +3,9 @@
 Simulator::Simulator(int w, int h, int nL, int nP) : w(w), h(h), nLandmarks(nL), nParticles(nP)
 {
     initLandmarks();
+    initParticles();
+    robot = Robot(cv::Point(w/2, h/2), 0);
+    std::cout << particles << std::endl;
 }
 
 void Simulator::initLandmarks()
@@ -10,9 +13,20 @@ void Simulator::initLandmarks()
     srand(time(NULL));
     for (int i = 0; i < nLandmarks; i++)
     {
-        int x = std::rand() % w;
-        int y = std::rand() % h;
+        float x = w * (double)std::rand() / RAND_MAX;
+        float y = h * (double)std::rand() / RAND_MAX;
         landmarks.push_back(cv::Point(x, y));
+    }
+}
+
+void Simulator::initParticles()
+{
+    for (int i = 0; i < nParticles; i++)
+    {
+        float x = w * (double)std::rand() / RAND_MAX;
+        float y = h * (double)std::rand() / RAND_MAX;
+        float yaw = 2 * M_PI * (double)std::rand() / RAND_MAX;
+        particles.push_back(Robot(cv::Point(x, y), yaw)); 
     }
 }
 
--- Localization/ParticleFilter/simulator.h
+++ Localization/ParticleFilter/simulator.h
@@ -6,18 +6,23 @@
 #include <vector>
 #include <opencv2/opencv.hpp>
 
+#include "robot.h"
+
 using std::vector;
 
 class Simulator
 {
-public:
-    Simulator(int, int, int, int);
-    void initLandmarks();
-    int run();
+    public:
+        Simulator(int, int, int, int);
+        void initLandmarks();
+        void initParticles();
+        int run();
 
-private:
-    int w, h, nLandmarks, nParticles;
-    vector<cv::Point> landmarks;
+    private:
+        int w, h, nLandmarks, nParticles;
+        Robot robot;
+        vector<cv::Point> landmarks;
+        vector<Robot> particles;
 };
 
 #endif
