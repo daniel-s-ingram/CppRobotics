f5a184db79cc3bb56dc68725348b37ff07f24f52
Daniel Ingram <ingramds@appstate.edu>
2019-03-07 16:21:54 -0500

    Set up matrices for Kalman filter

--- Localization/KalmanFilter/filter.cpp
+++ Localization/KalmanFilter/filter.cpp
@@ -1,1 +1,42 @@
 #include "filter.h"
+
+Kalman::Kalman(float initX, float initY, float dt, const vector<float>& stateVar, const vector<float>& measVar)
+{
+    //Identity matrix
+    I << 1, 0, 0, 0,
+         0, 1, 0, 0,
+         0, 0, 1, 0,
+         0, 0, 0, 1;
+
+    //State transition matrix
+    F << 1, 0, dt,  0,
+         0, 1,  0, dt,
+         0, 0,  1,  0,
+         0, 0,  0,  1;
+
+    //Measurement function
+    H << 1, 0, 0, 0,
+         0, 1, 0, 0;
+
+    //Measurement covariance
+    R << measVar[0],          0,
+                  0, measVar[1];
+
+    //State covariance
+    P << stateVar[0],           0,           0,           0,
+                   0, stateVar[1],           0,           0,
+                   0,           0, stateVar[2],           0,
+                   0,           0,           0, stateVar[3];
+
+    //State estimate
+    x << initX,
+         initY,
+             0,
+             0;
+
+    //Control inputs
+    u << 0,
+         0,
+         0,
+         0;
+}
--- Localization/KalmanFilter/filter.h
+++ Localization/KalmanFilter/filter.h
@@ -1,4 +1,22 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
+#include <vector>
+#include <eigen3/Eigen/Core>
+
+using std::vector;
+
+class Kalman
+{
+    public:
+        Kalman() {};
+        Kalman(float, float, float, const vector<float>&, const vector<float>&);
+
+    private:
+        Eigen::Matrix<float, 4, 4> I, F, P;
+        Eigen::Matrix<float, 2, 4> H;
+        Eigen::Matrix<float, 2, 2> R;
+        Eigen::Matrix<float, 4, 1> x, u;
+};
+
 #endif
